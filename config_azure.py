af4943bfe4214813a9486d045e6492268c62271f
Iva <i.gornishka@amsterdam.nl>
2024-03-13 16:55:26 +0000

    added configs

--- config_azure.py
+++ config_azure.py
@@ -1,1 +1,13 @@
 """Paths when running on Azure (AML)"""
+
+HUGGING_CACHE = "/home/azureuser/cloudfiles/code//hugging_cache"
+
+# Main folders
+in_folder = "/home/azureuser/cloudfiles/code/blobfuse/raadsinformatie"
+out_folder = "/home/azureuser/cloudfiles/code/blobfuse/raadsinformatie/processed_data/woo_qa"
+
+woo_sources = {
+    "raadsinformatie": f"{in_folder}/raadsinformatie/search_results/woo_queries/",
+    "openamsterdam": f"{in_folder}/open.amsterdam/woo_queries/",
+    "amsterdam.nl": f"{in_folder}/amsterdam.nl/",
+}
--- settings.py
+++ settings.py
@@ -1,1 +1,3 @@
 """Settings that or not paths or secrets"""
+
+AZURE_OPENAI_ENDPOINT = "https://ai-openai-ont.openai.azure.com/"
