d3baf047c57e9ea81ca4f9e46b069c5878d2f071
Iva <i.gornishka@amsterdam.nl>
2024-07-22 16:39:48 +0000

    add data

---
+++ config.py
@@ -0,0 +1,11 @@
+"""Paths when running on Azure (AML)"""
+
+# Main folders
+raadsinformatie_in_folder = "../data"
+raadsinformatie_out_folder = "../output"
+
+woo_sources = {
+    "raadsinformatie": f"{raadsinformatie_in_folder}/raadsinformatie/",
+    "openamsterdam": f"{raadsinformatie_in_folder}/open.amsterdam/",
+    "amsterdam.nl": f"{raadsinformatie_in_folder}/amsterdam.nl/",
+}
--- config_azure.py
+++ config_azure.py
@@ -2,8 +2,6 @@
 
 HUGGING_CACHE = "/home/azureuser/cloudfiles/code//hugging_cache"
 
-chat_amsterdam_pilot_folder = "/home/azureuser/cloudfiles/code/blobfuse/generative_ai/chat_amsterdam/raw_data/pilot_prompts/"
-
 # Main folders
 raadsinformatie_in_folder = "/home/azureuser/cloudfiles/code/blobfuse/raadsinformatie"
 raadsinformatie_out_folder = "/home/azureuser/cloudfiles/code/blobfuse/raadsinformatie/processed_data/woo_qa"
